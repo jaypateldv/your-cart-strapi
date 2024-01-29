eeb1a88783f8972f8b8ec36e6be1bf1f3560b09b
jaypatel_simform <jaypatel@simformsolutions.com>
2024-01-29 10:16:08 +0530

    feat: done setup for google sso login in admin panel

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,47 @@
+const GoogleStrategy = require("passport-google-oauth2");
+
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    events:{
+      onConnectionSuccess(e) {},
+        onConnectionError(e) {},
+        // ...
+        onSSOAutoRegistration(e) {
+          const { user, provider } = e;
+
+          console.log(
+            `A new user (${user.id}) has been automatically registered using ${provider}`
+          );
+        },
+    },
+    providers: [
+      {
+        uid: "google",
+        displayName: "Google",
+        icon: "https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Google-512.png",
+        createStrategy: (strapi) =>
+          new GoogleStrategy.Strategy(
+            {
+              clientID: env("764416061831-h3ih952hhu6inf1c6v2gdsve9ogvlvsj.apps.googleusercontent.com"),
+              clientSecret: env("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"),
+              scope: [
+                "https://www.googleapis.com/auth/userinfo.email",
+                "https://www.googleapis.com/auth/userinfo.profile",
+              ],
+              callbackURL:
+                strapi.admin.services.passport.getStrategyCallbackURL("google"),
+            },
+            (request, accessToken, refreshToken, profile, done) => {
+              done(null, {
+                email: profile.email,
+                firstname: profile.given_name,
+                lastname: profile.family_name,
+              });
+            }
+          ),
+      },
+    ],
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
@@ -14,4 +55,5 @@ module.exports = ({ env }) => ({
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
+
 });
