9a704d5bfcc31077324743c40e6cdbd637d4724e
jaypatel_simform <jaypatel@simformsolutions.com>
2024-01-31 14:44:16 +0530

    feat: implement CRUD API for todo with specific user permission auth

--- src/api/todo/controllers/todo.js
+++ src/api/todo/controllers/todo.js
@@ -8,14 +8,47 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::todo.todo', {
 
-  // async find(ctx) {
-  //   await this.validateQuery(ctx);
-  //   let sanitizedQueryParams = await this.sanitizeQuery(ctx);
-  //   const userId = ctx.state.user.id;
-  //   sanitizedQueryParams = { ...sanitizedQueryParams, 'user.id': userId };
-  //   const { results, pagination } = await strapi.service('api::todo.todo').find(sanitizedQueryParams);
-  //   // console.log("res", results, pagination);
-  //   return this.transformResponse(results, { pagination });
-  // }
+  async create(ctx) {
+    await this.validateQuery(ctx);
+    console.log(ctx.request.body);
+    let sanitizedQueryParams = await this.sanitizeQuery(ctx);
+    const userId = ctx.state.user.id;
+    const data = { ...ctx.request.body['data'], user: userId };
+    const results = await strapi.service('api::todo.todo').create({ data });
+    return this.transformResponse(results);
+  },
+  async find(ctx) {
+    await this.validateQuery(ctx);
+    let sanitizedQueryParams = await this.sanitizeQuery(ctx);
+    const userId = ctx.state.user.id;
+    sanitizedQueryParams = { ...sanitizedQueryParams, user: { id: userId } };
+    const { results, pagination } = await strapi.service('api::todo.todo').find(sanitizedQueryParams);
+    return this.transformResponse(results, { pagination });
+  },
+
+  async update(ctx) {
+    await this.validateQuery(ctx);
+    let sanitizedQueryParams = await this.sanitizeQuery(ctx);
+    const userId = ctx.state.user.id;
+    const todo = await strapi.service('api::todo.todo').findOne(ctx.params.id, { populate: { user: true } });
+    if (todo.user.id !== userId)
+      return ctx.badRequest('Todo not found to update');
+    sanitizedQueryParams = { ...sanitizedQueryParams, user: { id: userId } };
+    const results = await strapi.service('api::todo.todo').update(ctx.params.id, ctx.request.body);
+    return this.transformResponse(results);
+  },
+
+  async delete(ctx) {
+    await this.validateQuery(ctx);
+    let sanitizedQueryParams = await this.sanitizeQuery(ctx);
+    const userId = ctx.state.user.id;
+    const todo = await strapi.service('api::todo.todo').findOne(ctx.params.id, { populate: { user: true } });
+    if (todo.user.id !== userId)
+      return ctx.badRequest('Todo not found to update');
+    sanitizedQueryParams = { ...sanitizedQueryParams, user: { id: userId } };
+    const results = await strapi.service('api::todo.todo').delete(ctx.params.id);
+    return this.transformResponse(results);
+  }
+
 
 });
--- src/api/todo/services/todo.js
+++ src/api/todo/services/todo.js
@@ -7,20 +7,18 @@
 const { createCoreService } = require('@strapi/strapi').factories;
 
 module.exports = createCoreService('api::todo.todo', {
-  // async find(...args) {
-  //   console.log({ ...args });
-  //   // Calling the default core controller
-  //   const { results, pagination } = await super.find({
-  //     filters: { ...args[0], 'user.id': 6 },
-  //     populate: { user: true }
-  //   });
+  async find(...args) {
+    const { results, pagination } = await super.find({
+      filters: args[0],
+      populate: { user: true }
+    });
 
-  //   // some custom logic
-  //   results.forEach(result => {
-  //     result.counter = 1;
-  //   });
+    results.forEach(result => {
+      result.counter = 1;
+    });
 
-  //   return { results, pagination };
-  // },
+    return { results, pagination };
+  },
+
 });
 
